9077b31999486774342b32e687d1a7a5ad94f707
AmpElectrecuted <AmpElectrecuted@users.noreply.github.com>
2025-02-01 17:22:00 +0000

    Format code

--- addons/jump-to-unread/userscript.js
+++ addons/jump-to-unread/userscript.js
@@ -1,19 +1,19 @@
 // This is technically implemented in Scratch, there is just a separate link
 // in the subforum page. This script replaces topic title with the unread link
 
-document.addEventListener('DOMContentLoaded', function() {
-    const containers = document.querySelectorAll('.tclcon');
-    containers.forEach(container => {
-        const links = container.querySelectorAll('a');
-        links.forEach(link => {
-            link.href += '/unread';
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const containers = document.querySelectorAll(".tclcon");
+  containers.forEach((container) => {
+    const links = container.querySelectorAll("a");
+    links.forEach((link) => {
+      link.href += "/unread";
     });
+  });
 
-    const tclLinks = document.querySelectorAll('.tcl a');
-    tclLinks.forEach(link => {
-        if (!link.closest('.tclcon')) {
-            link.remove();
-        }
-    });
+  const tclLinks = document.querySelectorAll(".tcl a");
+  tclLinks.forEach((link) => {
+    if (!link.closest(".tclcon")) {
+      link.remove();
+    }
+  });
 });
