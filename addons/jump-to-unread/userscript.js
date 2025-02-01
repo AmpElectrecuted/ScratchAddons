d1c69e4909ea573a46d982ff891a68ff3da30d85
AmpElectrecuted <AmpElectrecuted@users.noreply.github.com>
2025-02-01 17:42:58 +0000

    Format code

--- addons/jump-to-unread/userscript.js
+++ addons/jump-to-unread/userscript.js
@@ -2,18 +2,18 @@
    in the subforum page. This script replaces topic title with the unread link */
 
 export default async function ({ addon, console }) {
-    const containers = document.querySelectorAll('.tclcon');
-    containers.forEach(container => {
-        const links = container.querySelectorAll('a');
-        links.forEach(link => {
-            link.href += '/unread';
-        });
+  const containers = document.querySelectorAll(".tclcon");
+  containers.forEach((container) => {
+    const links = container.querySelectorAll("a");
+    links.forEach((link) => {
+      link.href += "/unread";
     });
+  });
 
-    const tclLinks = document.querySelectorAll('.tcl a');
-    tclLinks.forEach(link => {
-        if (!link.closest('.tclcon')) {
-            link.remove();
-        }
-    });
-  }
+  const tclLinks = document.querySelectorAll(".tcl a");
+  tclLinks.forEach((link) => {
+    if (!link.closest(".tclcon")) {
+      link.remove();
+    }
+  });
+}
